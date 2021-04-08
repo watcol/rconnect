2d28da9c260f0aee8e84734a4a3f33efc2f6c1fd
watcol <potfman@gmail.com>
2021-04-08 16:03:52 +0900

    Add packet parser.

--- rconnect-cli/src/main.rs
+++ rconnect-cli/src/main.rs
@@ -1,4 +1,59 @@
 extern crate anyhow;
+extern crate serde;
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct Packet {
+    id: u128,
+    #[serde(flatten)]
+    packet_type: PacketType,
+}
+
+impl Packet {
+    fn from_json(s: &str) -> serde_json::Result<Self> {
+        serde_json::from_str(s)
+    }
+
+    fn to_json(&self) -> serde_json::Result<String> {
+        serde_json::to_string(self)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "body")]
+enum PacketType {
+    #[serde(rename = "kdeconnect.identity")]
+    Identity(Identity),
+    #[serde(rename = "kdeconnect.pair")]
+    Pair(JsonValue),
+    #[serde(rename = "kdeconnect.encrypted")]
+    Encrypted(JsonValue),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Identity {
+    device_name: String,
+    device_type: DeviceType,
+    protocol_version: u8,
+    tcp_port: u16,
+    incoming_capabilities: Vec<String>,
+    outgoing_capabilities: Vec<String>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+enum DeviceType {
+    Desktop,
+    Laptop,
+    #[serde(alias = "smartphone")]
+    Phone,
+    Tablet,
+    Tv,
+}
 
 use std::net::UdpSocket;
 
@@ -9,7 +64,9 @@ fn discover() -> anyhow::Result<()> {
     let mut buf = [0; 10000];
     let (size, addr) = socket.recv_from(&mut buf)?;
     println!("received {} bytes from {}:", size, addr);
-    println!("{}", String::from_utf8_lossy(&buf));
+    let buf = std::str::from_utf8(&buf)?.trim_matches('\u{0}');
+    let packet = Packet::from_json(buf)?;
+    println!("{:#?}", packet);
 
     Ok(())
 }
