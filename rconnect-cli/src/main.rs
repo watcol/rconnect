e953d42963444f50e3604968d61abf67269aec41
watcol <potfman@gmail.com>
2021-04-08 16:42:13 +0900

    Support sending "identity" packet.

--- rconnect-cli/src/main.rs
+++ rconnect-cli/src/main.rs
@@ -1,9 +1,33 @@
 extern crate anyhow;
+extern crate dns_lookup;
 extern crate serde;
 extern crate serde_json;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Value as JsonValue;
+
+static PORT: u16 = 1716;
+#[cfg(any(target_os = "ios", target_os = "android"))]
+static DEVICE_TYPE: DeviceType = DeviceType::Phone;
+#[cfg(not(any(target_os = "ios", target_os = "android")))]
+static DEVICE_TYPE: DeviceType = DeviceType::Desktop;
+
+static PROTOCOL_VERSION: u8 = 7;
+
+fn gen_id() -> u128 {
+    use std::time::{SystemTime, UNIX_EPOCH};
+    match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(d) => d.as_millis(),
+        Err(_) => panic!("Failed to generate id."),
+    }
+}
+
+fn device_name() -> String {
+    match dns_lookup::get_hostname() {
+        Ok(hostname) => hostname,
+        Err(_) => String::from("fallback"),
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 struct Packet {
@@ -20,6 +44,13 @@ impl Packet {
     fn to_json(&self) -> serde_json::Result<String> {
         serde_json::to_string(self)
     }
+
+    fn new_identity() -> Self {
+        Self {
+            id: gen_id(),
+            packet_type: PacketType::new_identity(),
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -33,6 +64,12 @@ enum PacketType {
     Encrypted(JsonValue),
 }
 
+impl PacketType {
+    fn new_identity() -> Self {
+        Self::Identity(Identity::new())
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Identity {
@@ -44,7 +81,20 @@ struct Identity {
     outgoing_capabilities: Vec<String>,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+impl Identity {
+    fn new() -> Self {
+        Self {
+            device_name: device_name(),
+            device_type: DEVICE_TYPE,
+            protocol_version: PROTOCOL_VERSION,
+            tcp_port: PORT,
+            incoming_capabilities: Vec::new(),
+            outgoing_capabilities: Vec::new(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 enum DeviceType {
     Desktop,
@@ -58,16 +108,23 @@ enum DeviceType {
 use std::net::UdpSocket;
 
 fn discover() -> anyhow::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:1716")?;
+    let socket = UdpSocket::bind(("0.0.0.0", PORT))?;
+    let local_addr = socket.local_addr()?;
     socket.set_read_timeout(Some(std::time::Duration::from_secs(60)))?;
     socket.set_broadcast(true)?;
-    let mut buf = [0; 10000];
-    let (size, addr) = socket.recv_from(&mut buf)?;
+    let send_packet = Packet::new_identity().to_json()?;
+    socket.send_to(send_packet.as_bytes(), ("255.255.255.255", PORT))?;
+    let (size, addr, buf) = loop {
+        let mut buf = [0; 10000];
+        socket.recv_from(&mut [])?;
+        let (size, addr) = socket.recv_from(&mut buf)?;
+        if addr != local_addr {
+            break (size, addr, buf);
+        }
+    };
+    let received_packet = Packet::from_json(std::str::from_utf8(&buf)?.trim_matches('\u{0}'))?;
     println!("received {} bytes from {}:", size, addr);
-    let buf = std::str::from_utf8(&buf)?.trim_matches('\u{0}');
-    let packet = Packet::from_json(buf)?;
-    println!("{:#?}", packet);
-
+    println!("{:#?}", received_packet);
     Ok(())
 }
 
